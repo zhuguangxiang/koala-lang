bf6dd180e982378be0a40416ac34bde8594a9723
zhuguangxiang <zhuguangxiang@163.com>
2018-02-12 22:02:44 +0800

    omit last

--- test_routine.c
+++ test_routine.c
@@ -6,18 +6,18 @@ void test_routine(void)
   Object *lang = Koala_Get_Module("koala/lang");
   Klass *klazz = Module_Get_Class(lang, "Tuple");
   ASSERT_PTR(klazz);
-  Object *method = Klass_Get_Method(klazz, "Size");
+  Object *code = Klass_Get_Method(klazz, "Size");
   Object *tuple = Tuple_New(10);
-  Routine *rt = Routine_New(method, tuple, NULL);
+  Routine *rt = Routine_New(code, tuple, NULL);
   Routine_Run(rt, PRIO_NORMAL);
   while (Routine_State(rt) != STATE_DEAD);
-  TValue val = ValueStack_Pop(&rt->stack);
+  TValue val = rt_stack_pop(rt);
   ASSERT(VALUE_ISINT(&val));
   printf("size:%lld\n", VALUE_INT(&val));
   assert(VALUE_INT(&val) == 10);
-  assert(ValueStack_Size(&rt->stack) == 0);
+  assert(rt_stack_size(rt) == 0);
 
-  method = Klass_Get_Method(klazz, "Get");
+  code = Klass_Get_Method(klazz, "Get");
   setivalue(&val, 100);
   Tuple_Set(tuple, 0, &val);
   setbvalue(&val, 1);
@@ -27,26 +27,26 @@ void test_routine(void)
   setivalue(&val, 0);
   Tuple_Set(args, 0, &val);
 
-  rt = Routine_New(method, tuple, args);
+  rt = Routine_New(code, tuple, args);
   Routine_Run(rt, PRIO_NORMAL);
   while (Routine_State(rt) != STATE_DEAD);
-  val = ValueStack_Pop(&rt->stack);
+  val = rt_stack_pop(rt);
   ASSERT(VALUE_ISINT(&val));
   printf("int value:%lld\n", VALUE_INT(&val));
   assert(VALUE_INT(&val) == 100);
-  assert(ValueStack_Size(&rt->stack) == 0);
+  assert(rt_stack_size(rt) == 0);
 
   setivalue(&val, 1);
   Tuple_Set(args, 0, &val);
 
-  rt = Routine_New(method, tuple, args);
+  rt = Routine_New(code, tuple, args);
   Routine_Run(rt, PRIO_NORMAL);
   while (Routine_State(rt) != STATE_DEAD);
-  val = ValueStack_Pop(&rt->stack);
+  val = rt_stack_pop(rt);
   ASSERT(VALUE_ISBOOL(&val));
   printf("bool value:%d\n", VALUE_BOOL(&val));
   assert(VALUE_BOOL(&val) == 1);
-  assert(ValueStack_Size(&rt->stack) == 0);
+  assert(rt_stack_size(rt) == 0);
 }
 
 int main(int argc, char *argv[]) {
