6e38f5e8ee9b895e639a39bf0a714b58ad1b7168
zhuguangxiang <zhuguangxiang@163.com>
2018-03-11 01:40:23 +0800

    add properties support

---
+++ properties.c
@@ -0,0 +1,91 @@
+
+#include "properties.h"
+#include "hash.h"
+
+PropEntry *PropEntry_New(char *key)
+{
+	PropEntry *e = malloc(sizeof(PropEntry));
+	Init_HashNode(&e->hnode, e);
+	e->key = key;
+	return e;
+}
+
+static uint32 prop_hash(void *k)
+{
+	PropEntry *e = k;
+	return hash_string(e->key);
+}
+
+static int prop_equal(void *k1, void *k2)
+{
+	PropEntry *e1 = k1;
+	PropEntry *e2 = k2;
+	return !strcmp(e1->key, e2->key);
+}
+
+int Properties_Init(Properties *prop)
+{
+	HashInfo hashinfo;
+	Init_HashInfo(&hashinfo, prop_hash, prop_equal);
+	HashTable_Init(&prop->table, &hashinfo);
+	Vector_Init(&prop->vec);
+	return 0;
+}
+
+void Properties_Fini(Properties *prop)
+{
+	HashTable_Fini(&prop->table, NULL, NULL);
+	Vector_Fini(&prop->vec, NULL, NULL);
+}
+
+int Properties_Put(Properties *prop, char *key, char *val)
+{
+	PropEntry e = {.key = key};
+	PropEntry *entry = HashTable_Find(&prop->table, &e);
+	if (entry) {
+		if (entry->count == 1) {
+			char *v = Vector_Get(&prop->vec, entry->index);
+			Vector *vec = Vector_New();
+			Vector_Append(vec, v);
+			Vector_Append(vec, val);
+			Vector_Set(&prop->vec, entry->index, vec);
+			entry->count++;
+		} else {
+			Vector *vec = Vector_Get(&prop->vec, entry->index);
+			Vector_Append(vec, val);
+			entry->count++;
+		}
+	} else {
+		entry = PropEntry_New(key);
+		entry->count = 1;
+		entry->index = Vector_Size(&prop->vec);
+		HashTable_Insert(&prop->table, &entry->hnode);
+		Vector_Append(&prop->vec, val);
+	}
+
+	return 0;
+}
+
+PropEntry *Properties_Get(Properties *prop, char *key)
+{
+	PropEntry e = {.key = key};
+	return HashTable_Find(&prop->table, &e);
+}
+
+char *Properties_Next(Properties *prop, PropEntry *entry, int next)
+{
+	if (entry->count > 1) {
+		Vector *vec = Vector_Get(&prop->vec, entry->index);
+		if (next < entry->count) {
+			return Vector_Get(vec, next);
+		} else {
+			return NULL;
+		}
+	} else {
+		if (!next) {
+			return Vector_Get(&prop->vec, entry->index);
+		} else {
+			return NULL;
+		}
+	}
+}
---
+++ properties.h
@@ -0,0 +1,35 @@
+
+#ifndef _KOALA_PROPERTIES_H_
+#define _KOALA_PROPERTIES_H_
+
+#include "hashtable.h"
+#include "vector.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct prop_entry {
+	HashNode hnode;
+	char *key;
+	int index;
+	int count;
+} PropEntry;
+
+typedef struct properties {
+	HashTable table;
+	Vector vec;
+} Properties;
+
+Properties *Properties_New(void);
+void Properties_Free(Properties *prop);
+int Properties_Init(Properties *prop);
+void Properties_Fini(Properties *prop);
+int Properties_Put(Properties *prop, char *key, char *val);
+PropEntry *Properties_Get(Properties *prop, char *key);
+char *Properties_Next(Properties *prop, PropEntry *entry, int next);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _KOALA_PROPERTIES_H_ */
