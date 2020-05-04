ea751543eca897150c78ad373940d0905b80f0a7
zhuguangxiang <zhuguangxiang@163.com>
2020-05-04 12:10:30 +0800

    spacemacs support koala-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ((nil .
       ((cmake-ide-project-dir . "~/github.com/koala-lang")
        (cmake-ide-build-dir . "~/github.com/koala-lang/build")
-       (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug")
+       (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=NDebug")
        (helm-make-build-dir . "build")
        (helm-make-arguments . "-j1")
        (helm-ctest-dir . "~/github.com/koala-lang/build/")
